b92f35120bc446dca6fca4f89bc85465ae936237
paperclip <blankb223@gmail.com>
2020-11-27 15:27:28 +0800

    the commit alvin

---
+++ server/posts/post.service.js
@@ -0,0 +1,57 @@
+﻿//DITO NIYO ILALAGAY YUNG PINAKA LOGIC TALAGA WALANG ROUTING MGA FUNCTION LANG 
+
+
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const Post = db.Post;
+
+module.exports = {
+    getAllPost,
+    getPostById,
+    createPost,
+    updatePost,
+    deletePost
+};
+
+async function getAllPost() {
+    return await Post.find();
+}
+
+async function getPostById(id) {
+    return await Post.findById(id);
+}
+
+async function createPost(userParam, userid) {
+    // validate
+    const post = new Post({
+        user_id: userid,
+        user_name: userParam.username,
+        upvotes: userParam.upvotes,
+        status: userParam.status,
+        post_title: userParam.title,
+        post_description: userParam.description,
+        photos: userParam.photos,
+        tags: userParam.tags,
+        category: userParam.category
+    });
+    // save user
+    await post.save();
+}
+
+async function updatePost(id, userParam) {
+    const post = await Post.findById(id);
+
+    // validate
+    if (!post) throw 'Post not found';
+
+    // copy userParam properties to user
+    Object.assign(post, userParam);
+
+    await post.save();
+}
+
+async function deletePost(id) {
+    await Post.findByIdAndRemove(id);
+}
---
+++ server/posts/posts.controller.js
@@ -0,0 +1,47 @@
+﻿//DITO NIYO ILALAGAY YUNG MGA ROUTES TAPOS YUNG MGA FUNCTION NA GAGAMITIN NYA SA SERVICE
+const express = require('express');
+const router = express.Router();
+const postService = require('./post.service');
+
+// routes
+
+router.get('/', getAllPost);
+router.get('/:id', getPostById);
+// router.get('/popular', getAllsortByPopular);
+// router.get('/lastest', getAllsortBydatecreated);
+// router.get('/completed', getAllByStatus);
+router.post('/create', createPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+console.log('hello')
+module.exports = router;
+function getAllPost(req, res, next) {
+    postService.getAllPost()
+        .then(posts => res.json(posts))
+        .catch(err => next(err));
+}
+
+function createPost(req, res, next) {
+    postService.createPost(req.body , req.user.sub)
+        .then(() => res.json({
+            "registered":"true"
+        }))
+        .catch(err => next(err));
+}
+function getPostById(req, res, next) {
+    postService.getPostById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function updatePost(req, res, next) {
+    postService.updatePost(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function deletePost(req, res, next) {
+    postService.deletePost(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
