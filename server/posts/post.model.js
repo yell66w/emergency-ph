8adad60589215b292f930c087dca247cb5aac1c6
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-27 20:23:07 +0800

    looped through tags

--- server/posts/post.model.js
+++ server/posts/post.model.js
@@ -1,32 +1,30 @@
 //AMO INI IT MODEL HIN USER HA DATABASE
 
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 //ITO KAILANGAN NIYO SA PAG REGISTER EXCPET SA HASH AND CREATED DATE
 const schema = new Schema({
-    user_id: { type: String, required: true },
-    user_name: { type: String, required: true },
-    created_date: { type: Date, default: Date.now },
-    updatedDate: { type: Date , default: Date.now},
-    upvotes: {type: Number},
-    status: { type: String, enum : ['PENDING','RESCUED'],  default: 'PENDING'},
-    post_title:{ type: String, required: true },
-	post_description:{ type: String, required: true },
-	photos:{type:Array},
-	tags:{type:Array},
-	category: { type: String, enum : ['TYPHOON','FIRE','EARTHQUAKE','CRIME'] },
+  user_id: { type: String, required: true },
+  user_name: { type: String, required: true },
+  created_date: { type: Date, default: Date.now },
+  updatedDate: { type: Date, default: Date.now },
+  upvotes: { type: Number },
+  status: { type: String, enum: ["PENDING", "RESCUED"], default: "PENDING" },
+  post_title: { type: String, required: true },
+  post_description: { type: String, required: true },
+  photos: [],
+  tags: [],
+  category: { type: String, enum: ["TYPHOON", "FIRE", "EARTHQUAKE", "CRIME"] },
 });
 
-schema.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        delete ret._id;
-        delete ret.hash;
-    }
+schema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+    delete ret.hash;
+  },
 });
 
-module.exports = mongoose.model('Post', schema);
+module.exports = mongoose.model("Post", schema);
