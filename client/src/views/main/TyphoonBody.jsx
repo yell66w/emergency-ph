91270e03caa7b31baf3bbc027ec63aa465f9ba6b
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-28 14:09:43 +0800

    Fixed typhoon body

--- client/src/views/main/TyphoonBody.jsx
+++ client/src/views/main/TyphoonBody.jsx
@@ -1,37 +1,18 @@
 import React, { useEffect, useState } from "react";
-import Modal from "../../components/misc/Modal";
 import Post from "../../components/posts/Post";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { postSchema } from "../../models/PostSchema";
 import Spinner from "react-spinners/MoonLoader";
 import { PostService } from "../../services/PostService";
 import CreatePost from "../../components/misc/CreatePost";
 import SortTab from "../../components/misc/SortTab";
+import CreatePostModal from "../../components/misc/CreatePostModal";
 
 const TyphoonBody = ({ currentUser }) => {
   const _post = new PostService();
   const [posts, setPosts] = useState([]);
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(postSchema),
-  });
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [postUpdated, setPostUpdated] = useState(true);
   const [sortedBy, setSortedBy] = useState("LATEST");
-
-  const onSubmit = async (data) => {
-    setIsSubmitting(true);
-    try {
-      await _post.createPost(data);
-      setPostUpdated(true);
-      setShowModal(false);
-    } catch (error) {
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
 
   useEffect(() => {
     const getAllPosts = async () => {
@@ -68,63 +49,11 @@ const TyphoonBody = ({ currentUser }) => {
         </div>
       </div>
       {/* Modal */}
-      <Modal
+      <CreatePostModal
         showModal={showModal}
         setShowModal={setShowModal}
-        contentLabel="AboutModal"
-      >
-        <div className="flex flex-col justify-center">
-          <h1 className="font-bold text-2xl text-center">Ask for Help</h1>
-          <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-            <textarea
-              className={`
-            ${errors.description ? "border border-red-600" : null} 
-            focus:outline-none h-64 focus:bg-gray-200  font-medium rounded-lg bg-gray-100 w-full px-6 py-4 text-sm mt-4`}
-              type="text"
-              placeholder="What's wrong?"
-              name="description"
-              rows="4"
-              cols="50"
-              ref={register}
-            />
-            {errors.description ? (
-              <small className="text-xs w-2/5 text-red-600 font-medium my-1">
-                {errors.description?.message}
-              </small>
-            ) : null}
-            <select
-              className={`
-            ${errors.category ? "border border-red-600" : null} 
-            focus:outline-none focus:bg-gray-200  font-medium rounded-lg bg-gray-100 w-full h-14 px-6 py-4 text-sm mt-4`}
-              type=""
-              placeholder="Category"
-              name="category"
-              ref={register}
-            >
-              <option value="TYPHOON">Typhoon</option>
-              <option value="FIRE">Fire</option>
-              <option value="EARTHQUAKE">Earthquake</option>
-              <option value="CRIME">Crime</option>
-            </select>
-            {errors.category ? (
-              <small className="text-xs w-2/5 text-red-600 font-medium my-1">
-                {errors.category?.message}
-              </small>
-            ) : null}
-
-            <button
-              className="transition duration-500 w-full flex justify-center ease-in-out focus:bg-red-600 bg-red-500 focus:outline-none hover:bg-red-600 mt-9 cursor-pointer text-white font-semibold tracking-widest px-16 py-3 rounded-lg text-sm"
-              type="submit"
-            >
-              {isSubmitting ? (
-                <Spinner color={"#fff"} size={15}></Spinner>
-              ) : (
-                "SUBMIT"
-              )}
-            </button>
-          </form>
-        </div>
-      </Modal>
+        setPostUpdated={setPostUpdated}
+      />
     </>
   );
 };
